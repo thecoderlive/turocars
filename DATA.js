c070e04736172d278ba322287cbe9e24548307b5
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-02-01 15:32:07 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,65 @@
+export const item = {
+    "id": "62d96616-f847-445d-86b3-bd716590b062",
+    "turo_list": [
+        {
+            "id": "3f13bfc9-fa87-4251-88ca-731841405e96",
+            "car_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/32.png",
+            "heart_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/162.png",
+            "price_per_day": "term",
+            "car_name": "fall",
+            "year": "herself",
+            "star_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/366.png",
+            "tip_count": "send",
+            "book_instantly": "poor",
+            "free_delivery": "open"
+        },
+        {
+            "id": "8b9bb587-21a8-471e-9020-be7b92f288f1",
+            "car_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/280.png",
+            "heart_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/183.png",
+            "price_per_day": "century",
+            "car_name": "image",
+            "year": "it",
+            "star_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/204.png",
+            "tip_count": "institution",
+            "book_instantly": "visit",
+            "free_delivery": "market"
+        },
+        {
+            "id": "b72782d4-80bb-4973-b1c9-294f4d4e0fcd",
+            "car_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/139.png",
+            "heart_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/361.png",
+            "price_per_day": "bill",
+            "car_name": "individual",
+            "year": "group",
+            "star_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/85.png",
+            "tip_count": "store",
+            "book_instantly": "instead",
+            "free_delivery": "responsibility"
+        },
+        {
+            "id": "6656365d-a148-4ec0-b463-9b106e738599",
+            "car_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/5.png",
+            "heart_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/269.png",
+            "price_per_day": "song",
+            "car_name": "base",
+            "year": "act",
+            "star_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/178.png",
+            "tip_count": "fine",
+            "book_instantly": "less",
+            "free_delivery": "particularly"
+        },
+        {
+            "id": "5a5ffde8-4b44-4232-a229-c00ba4339781",
+            "car_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/320.png",
+            "heart_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/78.png",
+            "price_per_day": "true",
+            "car_name": "join",
+            "year": "simple",
+            "star_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/20.png",
+            "tip_count": "door",
+            "book_instantly": "cultural",
+            "free_delivery": "water"
+        }
+    ]
+}
